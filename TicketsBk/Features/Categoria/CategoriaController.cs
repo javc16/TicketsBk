9759167195bb65ff442353355a6e2cfe3fed3197
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-06-19 01:30:06 -0600

    agregando servicios

---
+++ TicketsBk/Features/Categoria/CategoriaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketsBk.Features.Categorias
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+    }
+}
